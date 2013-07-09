4c01f258c27f98ca40455ae075e6e65bae28cc9a
MarcusQuigley <Marcusquigley@gmail.com>
2013-07-09 10:23:53 +0000

    first cut of hw3part2.1

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fileName = 'index.html'
+var fs= require('fs');
+var msg = new Buffer('hello','utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send('msg');
 });
 
 var port = process.env.PORT || 5000;
