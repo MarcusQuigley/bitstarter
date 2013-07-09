cb3bbad80b036f82056355176544d5d00ed8cd3b
MarcusQuigley <Marcusquigley@gmail.com>
2013-07-09 10:26:44 +0000

    removed hardcoded msg

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fileName = 'index.html'
 var fs= require('fs');
-var msg = new Buffer('hello','utf8');
+var msg = new Buffer(fs.readFileSync(fileName),'utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('msg');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
