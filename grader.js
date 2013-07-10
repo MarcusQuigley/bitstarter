321e042b3f187ea9eb61ab836ef1312e3bfa13ab
MarcusQuigley <Marcusquigley@gmail.com>
2013-07-10 21:00:08 +0000

    completion of HW3 part3

--- grader.js
+++ grader.js
@@ -1,74 +1,83 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var cmdr = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var rest = require('restler');
 var assertFileExists = function(inFile) {
-	var instr = inFile.toString();
-	if (!fs.existsSync(instr)) {
-		console.log("%s does not exist. Exiting.", instr);
-		process.Exit(1);
-	}
-	return instr;
+        var instr = inFile.toString();
+        if (!fs.existsSync(instr)) {
+                console.log("%s does not exist. Exiting.", instr);
+                process.Exit(1);
+        }
+        return instr;
 };
 
-var cheerioHtmlFile = function(htmlFile) {
-	return cheerio.load(fs.readFileSync(htmlFile));
+var getHtml = function(htmlFile) {
+        return fs.readFileSync(htmlFile);
 };
 
 var loadChecks = function(checksfile)  {
         return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
-	var checks = loadChecks(checksfile).sort();
-	var out={};
-	for (var ii in checks) {
-		var present = $(checks[ii]).length > 0;
-		out[checks[ii]] = present;
-	}
-	return out;
+var checkHtml = function(html, checksfile) {
+
+        $ = cheerio.load(html);
+        var checks = loadChecks(checksfile).sort();
+        var out={};
+        for (var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+        }
+        return out;
 };
+
+var restResponse = function(result, response) {
+        if (result instanceof Error) {
+                console.error('Error: ' + result)
+        } else {
+                var html = result;
+                gradeJson(html);
+        }
+};
+
+var gradeJson = function(html) {
+		var checkJson = checkHtml(html, cmdr.checks);
+                var outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+};
+
+var gradeJson = function(html) {
+		var checkJson = checkHtml(html, cmdr.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+                        console.log(outJson);
+}
 
 //clones a function
 var clone = function(fn) {
-	return fn.bind({});
+        return fn.bind({});
 };
 
 
 if (require.main == module) {
-	cmdr
-		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>',     'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-		.parse(process.argv);
-	var checkJson = checkHtmlFile(cmdr.file, cmdr.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+        cmdr
+                .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists),CHECKSFILE_DEFAULT)
+                .option('-f, --file <html_file>',     'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+                .option('-u, --url <url>',     'URL you want to check')
+                .parse(process.argv);
+
+	        var checkJson=null;
+                if (cmdr.url==null)
+                {
+                        var html = getHtml(cmdr.file);
+			gradeJson(html);			
+                } else {
+                        rest.get(cmdr.url).on('complete', restResponse);
+                }
+
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+        exports.checkHtmlFile = checkHtmlFile;
 }
